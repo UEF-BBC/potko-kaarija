255211104c4487f517d1147cd974cbacba92435a
spvaanan <sami.vaananen@uef.fi>
2020-07-16 23:21:02 +0300

    Aloitettu ohjauslogiikan teko

--- Potkokaarija/Potkokaarija.ino
+++ Potkokaarija/Potkokaarija.ino
@@ -19,7 +19,9 @@ Tavoite: Yhdellä käärijän kierroksella paali liikkuu 5cm +-2cm.
   https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/ 
 */
 
-#include "SR04.h" //US anturin lukukoodi
+#include "SR04.h" //US anturin lukukoodi, löytyy 
+//https://www.elegoo.com/tutorial/Elegoo%20The%20Most%20Complete%20Starter%20Kit%20for%20MEGA2560%20V2.0.2020.6.17.zip
+//US anturin alta
 
 #define TRIG_PIN 12 //US anturin trikkaus
 #define ECHO_PIN 10 //US anturin luku
@@ -32,11 +34,13 @@ long d_sylinteri; //US anturin mittaama lukema mm tarkkuudella.
 const byte interruptPin = 2; //Pinni
 volatile long hallcount = 0; //Magneettien ohituslaskuri
 unsigned long time = 0; 
+long num = 0;
 
 //Servot
 
 //Laskennan muuttujat
-
+float kierros = 0;
+float suhde = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -52,10 +56,29 @@ void loop() {
    d_sylinteri=sr04.Distance(); //US anturin mittaama lukema mm tarkkuudella.
       Serial.print(d_sylinteri);   Serial.println("mm");
 
+  while (num != hallcount)
+  {
+    num = hallcount;
+    kierros = hallcount/2;
+  }
+
+  //Sylinterin liikuttava 50mm/kierros +-20mm. 
+  suhde = d_sylinteri/kierros;
+    if ( d_sylinteri < 50*kierros - 20) {
+    //nopeuta sylinteriä tai hidasta käärijää
+    }
+    else if ( d_sylinteri > 50*kierros + 20) {
+    //hidasta sylinteriä tai nopeuta käärijää
+    }
+
+
+  //Kirjoita tuloksia
+  Serial.println(kierros);
+
 }
 
 void hallanturi() {
-    if ((millis() - time) > 3)
+    if ((millis() - time) > 100)
         hallcount ++;
-  time = millis();
+    time = millis();
 }
