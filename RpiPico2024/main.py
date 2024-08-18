c9b2b3c9dd21ec9de566d9e17ade75cb588cb367
Sami Väänänen <sami.vaananen@uef.fi>
2024-08-18 21:47:03 +0300

    u

--- RpiPico2024/main.py
+++ RpiPico2024/main.py
@@ -1,9 +1,40 @@
-from machine import Pin
-from time import sleep
+#Demo for Rpi pico joystick
+#
+#VRy to physical 32 i.e. GP27 i.e. ADC1
+#VRx to physical 31 i.e. GP26 i.e. ADC0
+#Switch to physical 29 i.e. GP22
 
-led = Pin('LED', Pin.OUT)
-print('Blinking LED Example')
+from machine import Pin, ADC
+import utime
+import wifi
+
+yAxis = ADC(Pin(27))
+xAxis = ADC(Pin(26))
+
+button = Pin(22,Pin.IN, Pin.PULL_UP)
+
+ip = wifi.connect_to_wifi()
+host = "raspberrypi5"
+hostip = wifi.get_host_ip(host)
+print(f"Host and IP: {host}, {hostip}")
 
 while True:
-  led.value(not led.value())
-  sleep(0.5) 
+    xValue = xAxis.read_u16() #values between 0 and 65535
+    yValue = yAxis.read_u16()
+    buttonValue= button.value()
+    print(str(xValue) +", " + str(yValue) + " -- " + str(buttonValue))
+ 
+   #Scale from 0-1
+    xValue = xValue/65535
+    yValue = yValue/65535
+ 
+    data = {'xValue': xValue, 'yValue': yValue, 'buttonValue': buttonValue}
+
+     
+    url = f"http://{hostip}:5000/echo"
+    print(f"URL is {url}")
+    print(f"data is {data}")
+    wifi.send_post_request(url, data)
+
+    utime.sleep(0.2)
+
